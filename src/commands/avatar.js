0cb47250f2b221dd9166500d2b68e13d102dfc9d
Robelo06 <robertbelotti06@gmail.com>
2023-11-01 14:12:21 +0100

    Update avatar.js

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { discord } = require('../../config.json');
 //
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,12 +7,24 @@ module.exports = {
     	.addUserOption(option=>option.setName('user').setDescription('The user\'s avatar to show').setRequired(true))
     	.setDMPermission(true),
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const userEmbed = new EmbedBuilder()
-        	.setColor(0x000fff)
-        	.setTitle(`${user.username}\'s avatar`)
-        	.setImage(`${user.displayAvatarURL({ dynamic: true, size: 4096 })}`)
-        	.setTimestamp();
-        interaction.reply({ embeds: [userEmbed] });
+        await interaction.deferReply();
+        const wait = require('node:timers/promises').setTimeout;
+        await wait(2000);
+        try {
+            const user = interaction.options.getUser('user');
+        	const userEmbed = new EmbedBuilder()
+        		.setColor(0x000fff)
+        		.setTitle(`${user.username}\'s avatar`)
+        		.setImage(`${user.displayAvatarURL({ dynamic: true, size: 4096 })}`)
+        		.setTimestamp();
+        	interaction.editReply({ embeds: [userEmbed] });
+        } catch (err) {
+            console.log(err);
+            const erbed = new Embedbuilder()
+            	.setColor(0xff0000)
+            	.setTitle('We\'re sorry, an error occurred!')
+        		.setDescription(`Please wait a few seconds and if the error persists, please contact the Development Team in our [Discord Server](${discord})`);
+            interaction.editReply({ embeds: [erbed] });
+        }
     }
 }
