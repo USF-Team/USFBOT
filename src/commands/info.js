516ebce0d4b4b78540651cd9ad7a26719edb4659
Robelo06 <robertbelotti06@gmail.com>
2023-11-01 14:17:39 +0100

    Update info.js

--- src/commands/info.js
+++ src/commands/info.js
@@ -7,13 +7,13 @@ module.exports = {
   	async execute(interaction) {
     	const infoEmbed = new EmbedBuilder()
       		.setTitle('USF BOT')
-      		.setDescription('The USF BOT is a Moderation, Utility and Management bot to help every servers with moderation and have features they need and to keep it safe and fun. You can find a guide in the Discord server and more informations on its own [Github](https://github.com/orgs/USF-Team/repositories) and [website](https://usfteam.pages.dev)! Made by the USF Team.')
+      		.setDescription('The USF BOT is a Moderation, Utility and Management bot created to help every Servers with Moderation and Management plus it has features they need and to keep them safe and fun. You can find a guide in the Discord server and more informations on its own [Github Repository](https://github.com/orgs/USF-Team/repositories) and the [USF Website](https://usfteam.pages.dev)! Made by the USF Development Team.')
       		.addFields(
           		{ name: 'Version', value: `${version}` },
           		{ name: 'Guilds', value: `${interaction.client.guilds.cache.size}`},
           		{ name: 'Total Members', value: `${interaction.client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)}`}
       		)
-      		.setColor(0xFF0000)
+      		.setColor(0x000FFF)
       		.setThumbnail('https://cdn.discordapp.com/icons/1086638377534754897/1f9299b5fcc56efdba49f1caddd02550.webp?size=2048')
       		.setTimestamp()
       		.setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: `${interaction.user.displayAvatarURL({size:32})}`});
